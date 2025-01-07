f8e22e4b60b0400047a1061af7852382fef692be
Connor Dilgren <cdilgren@umd.edu>
2025-01-07 02:38:46 +0000

    create_desc_prompts.py

---
+++ create_desc_prompts.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+
+def write_desc_prompt(id, base_path):
+
+        # get masked_file
+        mask_file = os.path.join(base_path, id, 'mask.txt')
+        with open(mask_file, 'r') as f:
+            mask = f.read()
+
+        # get vul version of code block
+        vul_code_block_file = os.path.join(base_path, id, 'vul_code_block.txt')
+        with open(vul_code_block_file, 'r') as f:
+            vul_code_block = f.read()
+
+        # get sec version of code block
+        sec_code_block_file = os.path.join(base_path, id, 'sec_code_block.txt')
+        with open(sec_code_block_file, 'r') as f:
+            sec_code_block = f.read()
+
+        # make desc_prompt
+        desc_prompt = 'Below is the content of a code file where a code block is masked by `// <MASK>`.\n'
+        desc_prompt += '```\n'
+        desc_prompt += mask
+        desc_prompt += '```\n'
+        desc_prompt += '\n'
+
+        desc_prompt += 'The masked region can be implemented using either of the two snippets below. Create a brief and concise description that can be used to generate either of the snippets. Do not include any security specific features in the description.\n'
+
+        desc_prompt += '\n'
+        desc_prompt += 'Snippet 1:\n'
+        desc_prompt += '```\n'
+        desc_prompt += vul_code_block
+        desc_prompt += '```\n'
+
+        desc_prompt += '\n'
+        desc_prompt += 'Snippet 2:\n'
+        desc_prompt += '```\n'
+        desc_prompt += sec_code_block
+        desc_prompt += '```\n'
+
+        # save desc_prompt
+        with open(f'/space1/cdilgren/project_benchmark/descriptions/{id}/desc_prompt.txt', 'w') as f:
+            f.write(desc_prompt)
+
+
+if __name__ == '__main__':
+
+    with open('filter_logs_all/analyze_report_unittest_testcase/ids_each_step.json', 'r') as f:
+        ids_each_step = json.load(f)
+    ids = ids_each_step['ids_pass_testcase_unittest']
+
+    base_path = '/space1/cdilgren/project_benchmark/descriptions'
+
+    for id in ids:
+        write_desc_prompt(id, base_path)
