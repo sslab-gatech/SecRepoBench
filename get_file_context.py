54ef5a90ca4f691a901935361249181e830fa607
Connor Dilgren <cdilgren@umd.edu>
2025-03-18 07:50:17 +0000

    context length limits

--- get_file_context.py
+++ get_file_context.py
@@ -1,52 +1,74 @@
-import tiktoken
 import os
 from tqdm import tqdm
 import json
+import tree_sitter_c as tsc
+import tree_sitter_cpp as tscpp
+from tree_sitter import Language, Parser
 
-def find_special_symbol_tokens(enc, tokens, special_symbol):
-    # Find the index where the special symbol starts
-    for i in range(len(tokens)):
-        if special_symbol in enc.decode(tokens[i:]):
-            if special_symbol not in enc.decode(tokens[i+1:]):
-                return i, i+len(enc.encode(special_symbol))-1
+from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+from mistral_common.protocol.instruct.messages import UserMessage
+from mistral_common.protocol.instruct.request import ChatCompletionRequest
 
-    # If the symbol is not found, return None
-    return None, None
+from constants import *
+from cwe_map import *
 
-def get_file_context(input_path):
-    with open(os.path.join(input_path, 'mask_perturbed.txt'), 'r') as file:
-        content = file.read()
 
-    with open(os.path.join(input_path, 'mask_func_perturbed.txt'), 'r') as output_file:
-        func = output_file.read()
+def get_c_cpp_file(base_path: str):
+    c_path = base_path + '.c'
+    cpp_path = base_path + '.cpp'
+    if os.path.exists(c_path):
+        path = c_path
+    elif os.path.exists(cpp_path):
+        path = cpp_path
+    else:
+        print(f'This file does not exist with a c or cpp extension: {base_path}')
+        return
+    with open(path, 'r') as f:
+        content = f.read()
+    return content
+
+
+def truncate_content(content):
+
+
+def get_file_context(input_path, context_len_limit):
+    content = get_c_cpp_file(input_path + '/mask_desc_perturbed')
 
     # Tokenize the content
-    enc = tiktoken.encoding_for_model("gpt-4o")
-    tokens = enc.encode(content)
+    tokenizer = MistralTokenizer.v3()
+    completion_request = ChatCompletionRequest(messages=[UserMessage(content=content)])    
+    tokens = tokenizer.encode_chat_completion(completion_request).tokens
     token_count = len(tokens)
 
-    # If token count is less than or equal to 20k, save directly
-    if token_count <= 20000:
-        with open(os.path.join(input_path, 'new-in-file.txt'), 'w') as output_file:
+    # If added in file context is less than or equal to limit, save directly
+    context_len = token_count - context_len_limit["mask_func_desc_perturbed_token_len"]
+    if context_len <= context_len_limit["context_len_limit"]:
+        with open(os.path.join(input_path, 'in-file.txt'), 'w') as output_file:
             output_file.write(content)
     else:
-        print(f'Token count for file {input_path} is {token_count}, truncating the content to 15k tokens')
-        reserved_token_num = len(enc.encode(func))
-        truncated_content = enc.decode(tokens[:20000-reserved_token_num])
-        with open(os.path.join(input_path, 'new-in-file.txt'), 'w') as output_file:
+        print(f'context_len for file {input_path} is {context_len} but has a limit of {context_len_limit["context_len_limit"]}, truncating the context len')
+        # if this is just a few -- do manually ! 132 cases, too many for manual
+        truncated_content = truncate_content(content)
+        with open(os.path.join(input_path, 'in-file.txt'), 'w') as output_file:
             output_file.write(truncated_content)
 
 def main():
     input_dir = 'descriptions'
-    with open('/home/yanjun/arvo-oss-fuzz-bench/ids/final_ids.txt', 'r') as f:
-        ids = f.readlines()
-        ids = sorted([int(id.strip()) for id in ids])
-    for id in tqdm(ids):
-        id = str(id)
+
+    with open('ids.txt', 'r') as f:
+        ids = f.read().splitlines()[1:]
+
+    with open("context_len_limits.json", 'r') as f:
+        context_len_limits = json.load(f)
+
+    for id in ids:
+        context_len_limit = context_len_limits[id]
+
         input_path = os.path.join(input_dir, id)
-        if os.path.exists(os.path.join(input_path, 'new-in-file.txt')):
-            os.remove(os.path.join(input_path, 'new-in-file.txt'))
-        get_file_context(input_path)
+        if os.path.exists(os.path.join(input_path, 'in-file.txt')):
+            os.remove(os.path.join(input_path, 'in-file.txt'))
+        get_file_context(input_path, context_len_limit)
+
 
 if __name__ == '__main__':
     main()
