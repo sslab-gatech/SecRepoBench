e936b24f642988504a9c9b2801ff12b13abc48a8
Connor Dilgren <cdilgren@umd.edu>
2025-03-22 21:50:55 +0000

    modified imagemagick's relevant unittest matching

---
+++ relevant_passing_unittests.py
@@ -0,0 +1,46 @@
+import json
+from collections import defaultdict
+
+with open('ids.txt', 'r') as f:
+    ids = f.read().splitlines()[1:]
+
+with open('relevant_unittests.json', 'r') as f:
+    relevant_unittests = json.load(f)
+
+with open('/data/oss-fuzz-bench/output/report.json', 'r') as f:
+    report = json.load(f)
+
+with open('filter_logs/cases.json', 'r') as f:
+    cases = json.load(f)
+
+# no_relevant_passing = defaultdict(list)
+# relevant_passing_unittest = {}
+# for id in ids:
+#     relevant = relevant_unittests[id]['relevant_unittests']
+#     passing = report[id]['unittest_sec']['pass']
+#     relevant_passing_unittest[id] = list(set(relevant).intersection(set(passing)))
+#     if len(relevant_passing_unittest[id]) == 0:
+#         project_name = cases[id]['project_name']
+#         no_relevant_passing[project_name].append(id)
+# with open('no_relevant_passing.json', 'w') as f:
+#     json.dump(no_relevant_passing, f, indent=4)
+
+# missing_names = defaultdict(list)
+# for id in ids:
+#     # relevant = relevant_unittests[id]['relevant_unittests']
+#     passing = report[id]['unittest_sec']['pass']
+#     # relevant_passing_unittest[id] = list(set(relevant).intersection(set(passing)))
+#     if len(passing) == 0:
+#         project_name = cases[id]['project_name']
+#         missing_names[project_name].append(id)
+# with open('missing_names.json', 'w') as f:
+#     json.dump(missing_names, f, indent=4)
+
+# # with open('relevant_passing_unittest.json', 'w') as f:
+# #     json.dump(relevant_passing_unittest, f, indent=4)
+
+id = '26327'
+snapshot = report[id]
+snapshot['relevant_unittests'] = relevant_unittests[id]['relevant_unittests']
+with open('snapshot.json', 'w') as f:
+    json.dump(snapshot, f, indent=4)
